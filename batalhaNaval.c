a1d1f23330fb5e08d5377bb0317d29f5e1ddf398
rhuanlafayette <124814931+rhuanlafayette@users.noreply.github.com>
2025-04-02 19:52:48 +0000

    Criação do tabuleiro, dos navios horizontal e vertical Finalização do desafio iniciante.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,6 +5,83 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
+//criação do tabuleiro 10x10, todos os valores 0 conforme solicitado
+    int tabuleiro[10][10] = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,}
+    };
+        
+    printf("         Tabuleiro: \n"); //printf titulo 'Tabuleiro' 
+    char linhacoluna[10] = {'A','B','C','D','E','F','G','H','I','J'}; // criação de Array para que cada coluna seja denominada por uma letra
+        for (int c = 0; c < 10; c++) { // criação do for para que o sistema imprima cada letra sem precisar criar 10 linhas de codigo
+         printf("  %c", linhacoluna[c]); // coloquei um espaço antes do %c para que cada letra ficasse separada e organizada
+        };
+        printf("\n"); //printf com \n para pular uma linha assim que as colunas forem definidas
+
+    int numerolinha[10] = {1,2,3,4,5,6,7,8,9,10}; //array para enumerar as linhas e dar sequência ao jogo
+        for (int l = 0; l < 10; l++) { // for externo para imprimir cada número de linha
+        printf("%d", numerolinha[l]);
+        for(int j = 0; j < 10; j++){ // for interno para imprimir o tabuleiro linha por linha
+            printf("%3d", tabuleiro[j][l]); //Utilizei o %3d para dar uma distância de modo que os valores ficassem embaixo de cada letra da coluna
+        }
+        printf("\n"); // printf para pular uma linha
+    };
+
+    int naviovertical[1] = {3}; // array criado para atualizar o tabuleiro com o navio vertical
+    int naviohorizontal[1] = {3}; // array criado para atualizar o tabuleiro com o navio horizontal
+    for (int n = 2; n < 5; n++) // for criado para atualizar os numeros no tabuleiro
+    { // Com a intenção de mudar C2,C3 e C4, o for alterou os campos 2,3 e 4 da linha C para 3
+        tabuleiro[n][6]= naviovertical[0];
+    };
+    for(int v = 1;v < 4; v++){ //Seguindo a mesma lógica, o for alterou os campos C7,D7e E7 para 3, criando o navio horizontal
+        tabuleiro[2][v] = naviohorizontal[0];
+    };
+    
+    printf("\n"); //printf para pular uma linha
+    printf("Tabuleiro Atualizado com navios: \n"); // inicio da impressão do novo tabuleiro com os navios
+
+    
+    for (int c = 0; c < 10; c++) { // repeti o código que imprime as colunas definidas no array ' linhacoluna'
+         printf("  %c", linhacoluna[c]);
+        };
+        printf("\n");
+    
+    
+    for (int l = 0; l < 10; l++) { //repeti o codigo que imprime o número das linhas e o tabuleiro linha por linha
+        printf("%d", numerolinha[l]);
+        for(int j = 0; j < 10; j++){
+            printf("%3d", tabuleiro[j][l]);
+        } // como os valores já foram alterados internamente no codigo, a repetir o código ele imprime o tabuleiro atualizado
+        printf("\n"); //pular linha
+    };
+    
+    
+        
+        
+   
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
